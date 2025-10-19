863b54eedb1e02a955c7d00cce16168794553ef8
Bluechamele0n <tor.andersson06@outlook.com>
2025-10-19 15:54:01 +0200

    webbupdate

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+echo '<!DOCTYPE html>';
+echo '<html lang="en">';
+echo  '<head>';
+echo '<title>Fiondell_DND</title>'
+echo '</head>'
+echo '<body>'
+echo '<p>If you are not redirected, <a href="welcome.html">click here</a>.</p>'
+echo '</body>'
+echo '</html>'
